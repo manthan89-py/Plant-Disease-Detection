45ca91afb2e99f439862f34615063aa7ee4d7ff6
Manthan Bhikadiya ♚ <61309101+manthan89-py@users.noreply.github.com>
2021-04-28 23:26:15 +0530

    Update app.py

--- Flask Deployed App/app.py
+++ Flask Deployed App/app.py
@@ -60,11 +60,13 @@ def submit():
         supplement_name = supplement_info['supplement name'][pred]
         supplement_image_url = supplement_info['supplement image'][pred]
         supplement_buy_link = supplement_info['buy link'][pred]
-        return render_template('submit.html' , title = title , desc = description , prevent = prevent , image_url = image_url , pred = pred ,sname = supplement_name , simage = supplement_image_url , buy_link = supplement_buy_link)
+        return render_template('submit.html' , title = title , desc = description , prevent = prevent , 
+                               image_url = image_url , pred = pred ,sname = supplement_name , simage = supplement_image_url , buy_link = supplement_buy_link)
 
 @app.route('/market', methods=['GET', 'POST'])
 def market():
-    return render_template('market.html', supplement_image = list(supplement_info['supplement image']),supplement_name = list(supplement_info['supplement name']), disease = list(disease_info['disease_name']), buy = list(supplement_info['buy link']))
+    return render_template('market.html', supplement_image = list(supplement_info['supplement image']),
+                           supplement_name = list(supplement_info['supplement name']), disease = list(disease_info['disease_name']), buy = list(supplement_info['buy link']))
 
 if __name__ == '__main__':
     app.run(debug=True)
